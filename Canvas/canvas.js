69cfbc0d65a56fa6b340533666d2c4a51264da9b
Paul Farrington <farro4069@gmail.com>
2021-12-27 15:58:18 +1000

    iPad scroll still?

--- Canvas/canvas.js
+++ Canvas/canvas.js
@@ -18,6 +18,7 @@ let direction = true;
 function draw(e) {
 	if(!isDrawing) return;
 	e.preventDefault(); 
+	console.log(e);
 	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 	ctx.beginPath();
 	ctx.moveTo(lastX, lastY);
